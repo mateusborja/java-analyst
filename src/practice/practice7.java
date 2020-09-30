111d12df5dbbfd324da0ee357329f1bf2c6073b1
Mateus Borja <mateus.borja@gmail.com>
2020-09-29 22:47:11 -0400

    java if else

---
+++ src/practice/practice7.java
@@ -0,0 +1,39 @@
+package practice;
+
+public class practice7 {
+	public static void main(String[] args) {
+		int var5 = 5;
+		
+		if (var5 <5) {
+			var5++;
+			System.out.println(var5);
+			
+		} else {
+			++var5;
+			System.out.println(var5);
+
+		}
+		
+		
+		int x = 50;
+		
+		if (x > 30) {
+			System.out.println("Numero maior que 30");
+		
+		} else {
+			System.out.println("Numero menor que 30");
+		}
+		
+		/*if (condition boolean) {
+		 * executable code true
+		 * }
+		 * 
+		 * if (condition boolean) {
+		 * executable code true
+		 * } else {
+		 * executable code false
+		 * }
+		 */
+	}
+
+}
