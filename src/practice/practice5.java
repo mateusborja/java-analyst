1c4985eaf5c778945c3e0b8840dd7df7b2d9d00e
Mateus Borja <mateus.borja@gmail.com>
2020-09-29 22:41:51 -0400

    char type

---
+++ src/practice/practice5.java
@@ -0,0 +1,20 @@
+package practice;
+
+public class practice5 {
+	public static void main(String[] args) {
+		int i = 129;
+		byte b = (byte)i;
+		
+		System.out.println(i);
+		System.out.println(b);
+		
+		/* bit significant
+		 * 00000000 00000000 00000000 10000001 int representation binary
+		 * 10000001 - byte representation binary
+		*/
+		
+		
+		
+	}
+
+}
---
+++ src/practice/practice6.java
@@ -0,0 +1,17 @@
+package practice;
+//char type
+public class practice6 {
+	public static void main(String[] args) {
+		/*
+		 * ASCII Code do 'A'
+		 */
+		char c = 'A'; 
+		char d = 54;
+
+		System.out.println(c);
+		System.out.println(d);
+		
+		
+	}
+
+}
