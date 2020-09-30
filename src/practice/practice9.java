b550e0083067875a032ba433e8a64d148f79a7d1
Mateus Borja <mateus.borja@gmail.com>
2020-09-29 23:04:33 -0400

    java switch

---
+++ src/practice/practice9.java
@@ -0,0 +1,32 @@
+package practice;
+
+//java switch
+public class practice9 {
+	public static void main(String[] args) {
+		int i = 10;
+		
+		switch (i) {
+
+		case 1: {
+			if(i < 10) {
+			System.out.println("case 1");
+			break;
+			}
+		}
+		
+		case 2: {
+			if (i > 10) {
+			System.out.println("case 2");
+			break;
+			}
+		}
+		//equal else structural if else
+		default:
+			if (i >=10) {
+			System.out.println("default");
+			
+			}
+		}
+	}
+
+}
