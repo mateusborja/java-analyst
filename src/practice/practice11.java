e1fc901809d08893a79f29739f1748af2c1bbb51
Mateus Borja <mateus.borja@gmail.com>
2020-09-29 23:25:37 -0400

    java break

---
+++ src/practice/practice11.java
@@ -0,0 +1,16 @@
+package practice;
+
+//java break
+public class practice11 {
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			if (i == 5) {
+				break;
+			}
+			
+			System.out.println(i);
+		}
+		
+	}
+
+}
