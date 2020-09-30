476a60a4ce03698772b49f8db572204ab083a6d5
Mateus Borja <mateus.borja@gmail.com>
2020-09-29 21:47:08 -0400

    New files and begin course

---
+++ src/practice/practice1.java
@@ -0,0 +1,15 @@
+package practice;
+
+//change variables values
+public class practice1 {
+
+	public static void main(String[] args) {
+		int contador = 20;
+		contador = contador + 1;
+		
+		
+		
+		
+		
+	}
+}
