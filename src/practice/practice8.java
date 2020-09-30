ce425e6814e827c5c6204b1eb9bef16b0ff8b481
Mateus Borja <mateus.borja@gmail.com>
2020-09-29 22:54:15 -0400

    ternary operator for if else

---
+++ src/practice/practice8.java
@@ -0,0 +1,22 @@
+package practice;
+
+//java if else
+public class practice8 {
+	public static void main(String[] args) {
+		int x = 50;
+		boolean r;
+		
+		if (x > 30) {
+			r = true;
+		} else {
+			r = false;
+		}
+		
+		//ternary operator
+		r = x > 30 ? true : false;
+		
+		System.out.println(x);
+		System.out.println(r);
+		
+	}
+}
