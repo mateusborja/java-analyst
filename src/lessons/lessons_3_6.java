fe2c6618818ce4ef27c5ecef8cbdf5bebf9cac5c
Mateus Borja <mateus.borja@gmail.com>
2020-10-01 19:40:30 -0400

    java switch expressions

---
+++ src/lessons/lessons_3_6.java
@@ -0,0 +1,47 @@
+package lessons;
+//switch expressions
+public class lessons_3_6 {
+	
+	public enum DiasdaSemana {
+		SEGUNDA, TERCA, QUARTA, QUINTA, SEXTA
+	}
+	
+	public enum FinaisdeSemana {
+		SABADO, DOMINGO
+	}
+	
+
+	
+	public static void main(String[] args) {
+		//DiasdaSemana d = DiasdaSemana.SEGUNDA;
+		DiasdaSemana d = DiasdaSemana.QUARTA;
+		
+		switch (d) {
+		
+		case SEGUNDA:
+			System.out.println("1");
+			break;
+			
+		case TERCA:
+			System.out.println("2");
+			
+		}
+		
+		//switch expression
+		switch (d) {
+		case SEGUNDA -> System.out.println("seg");
+		case TERCA -> System.out.println("2");
+		default -> System.out.println("outro");
+			
+		}
+		
+		int i = switch(d) {
+		case SEGUNDA -> 1;
+		case TERCA -> 2;
+		default -> 0;
+		};
+		
+		System.out.println(i);
+	}
+		
+}
---
+++ src/practice/practice14.java
@@ -0,0 +1,58 @@
+package practice;
+//switch expressions
+public class practice14 {
+	public enum DiasdaSemana {
+		SEGUNDA, TERCA, QUARTA, QUINTA, SEXTA
+	}
+	
+	public enum FinaisdeSemana {
+		SABADO, DOMINGO
+	}
+	
+	public static void main(String[] args) {
+		DiasdaSemana d = DiasdaSemana.SEGUNDA;
+		FinaisdeSemana f = FinaisdeSemana.DOMINGO;
+		
+		switch (d) {
+		case TERCA: 
+			System.out.println(2);
+			System.out.println("ter");
+			break;
+		
+		case SEGUNDA:
+			System.out.println(1);
+			System.out.println("seg");
+			break;
+		
+		default:
+			System.out.println("Outro dia");
+		}
+		
+		switch (f) {
+		
+		case SABADO:
+			System.out.println(6);
+			System.out.println("sab");
+			break;
+		
+		case DOMINGO:
+			System.out.println(7);
+			System.out.println("dom");
+			break;
+		
+		default: 
+			System.out.println("outro dia");
+		
+		}
+		
+		int i = switch (f) {
+		case SABADO -> 1;
+		case DOMINGO -> 2;
+		default -> 0;
+		};
+		
+		System.out.println(i);
+			
+	}
+
+}
