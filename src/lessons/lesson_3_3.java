201349adb0d0c7da4f27361c80588928c7582070
Mateus Borja <mateus.borja@gmail.com>
2020-10-01 17:37:39 -0400

    java type casting

---
+++ src/lessons/lesson_3_3.java
@@ -0,0 +1,23 @@
+package lessons;
+
+public class lesson_3_3 {
+	public static void main(String[] args) {
+		int x = 10;
+		
+		var z = 20; //variable var
+		
+		z = 30;
+		
+		boolean y = true;
+		var m = true;
+		y = true;
+		
+		System.out.println(x + "\n\n" + z + "\n\n" + y + "\n\n" + m + "\n\n" + y); //contatenating values
+		// x + "\n\n" + z + ...
+		
+		// \n\n two line breaks
+		
+		
+	}
+	
+}
---
+++ src/lessons/lesson_3_4.java
@@ -0,0 +1,19 @@
+package lessons;
+//type casting
+public class lesson_3_4 {
+	public static void main(String[] args) {
+		
+		int x = 10; // not casting
+		
+		//long y = 10; //casting implicit (value 10 is whole number no output
+		
+		long y = 10L;
+		
+		double z = 10; // 
+		
+		float w = 10;
+		System.out.println(x +"\n\n" + y + "\n\n" + z + "\n\n" + w);
+		
+	
+	}
+}
