b6542e47ce87d3fa96f2227c8b378866fc31ece5
Mateus Borja <mateus.borja@gmail.com>
2020-09-30 22:33:08 -0400

    work variables

---
+++ src/lessons/lesson_3_2.java
@@ -0,0 +1,42 @@
+package lessons;
+
+//variables
+public class lesson_3_2 {
+	public static void main(String[] args) {
+		//int number;
+		//number = 50;
+		
+		int number = 50;
+		int number2 = number + 5;
+		
+		
+		number = number -1;
+		number -= 1;
+		number--;
+		
+		number = number * 2;
+		number *= 2;
+		System.out.println(number);
+		System.out.println(number2);
+		
+		boolean b = true;
+		System.out.println(b);
+		
+		long l = 10L + 5;
+		System.out.println(l);
+		
+		int d = 5 * 2;
+		System.out.println(d);
+		
+		double e = 5 * 2.0;
+		System.out.println(e);
+		
+		double x = 0.0 / 0;
+		System.out.println(x); //imaginar number
+		
+		double y = 10.0 / 0;
+		System.out.println(y); // infinity result
+		
+	}
+
+}
