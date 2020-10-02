3589236e290586b520626cd57801e3147135e91b
Mateus Borja <mateus.borja@gmail.com>
2020-10-02 19:49:57 -0400

    java while and for

---
+++ src/practice/practice15.java
@@ -0,0 +1,36 @@
+package practice;
+//while do while and for
+public class practice15 {
+
+	public static void main(String[] args) {
+		//while with continue
+		int x = 0;
+		
+		while (x <10) {
+			x++;
+			
+			if (x == 5) {
+				continue;
+			}
+			
+			System.out.println(x + "\n");
+		}
+		
+		//for
+		int i;
+		int j;
+		int k;
+		int l;
+
+		for (i = 0, j = 10, k = 5, l = 8; i <= 5; i++, j--, k--, l++) {
+			System.out.println(i + " - " + j + " - " + k + " - " + l + "\n");
+		}
+		
+		for (int m = 5; m < 10; m++) {
+			System.out.println("\n" + m);
+		}
+		
+	}
+	
+	
+}
