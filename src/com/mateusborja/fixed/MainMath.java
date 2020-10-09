7029e355f3ea042e7755f629ad2929c8d431048a
mateus.borja <mateus.borja@gmail.com>
2020-10-09 19:21:10 -0400

    instance class and create methods

---
+++ src/com/mateusborja/fixed/MainMath.java
@@ -0,0 +1,18 @@
+//Main Math
+package com.mateusborja.fixed;
+
+
+public class MainMath {
+	public static void main(String[] args) {
+		
+		Math m1= new Math();
+		
+		m1.calcula(1.0, 12.0);
+		
+		
+		
+		System.out.println(m1.soma);
+
+	}
+	
+}
---
+++ src/com/mateusborja/fixed/Math.java
@@ -0,0 +1,16 @@
+//math sum
+package com.mateusborja.fixed;
+
+public class Math {
+	
+	double x;
+	double y;
+	double soma;
+	
+	public void calcula(double x, double y) {
+		soma = x + y;
+	}
+	
+
+
+}
