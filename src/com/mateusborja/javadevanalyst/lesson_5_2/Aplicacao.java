d66cd4a4227e2565330277e70df006064beda0c2
mateus.borja <mateus.borja@gmail.com>
2020-10-13 22:25:19 -0400

    java modifiers

---
+++ src/com/mateusborja/javadevanalyst/lesson_5_2/Aplicacao.java
@@ -0,0 +1,13 @@
+//java modifiers - acces modifier
+package com.mateusborja.javadevanalyst.lesson_5_2;
+
+public class Aplicacao {
+	
+	public static void main(String[] args) {
+		
+		ContaBancaria c1 = new ContaBancaria();
+		
+		
+	}
+
+}
---
+++ src/com/mateusborja/javadevanalyst/lesson_5_2/ContaBancaria.java
@@ -0,0 +1,44 @@
+/*java modifiers - acces modifier
+ * and methods get
+ * method set 
+ */
+package com.mateusborja.javadevanalyst.lesson_5_2;
+
+public class ContaBancaria {
+
+	// int numConta;
+	// boolean ativa;
+	// double saldo;
+
+	private int numConta;
+	private boolean ativa;
+	private double saldo;
+
+	public int getNumConta() {
+		return numConta;
+	}
+
+	public void setNumConta(int numConta) {
+		//rules
+		if (numConta > 0) {
+			this.numConta = numConta;
+		}
+	}
+
+	public boolean isAtiva() {
+		return ativa;
+	}
+
+	public void setAtiva(boolean ativa) {
+		this.ativa = ativa;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+}
