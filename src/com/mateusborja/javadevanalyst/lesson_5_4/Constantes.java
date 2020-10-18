05f97f4b1a5857d4d4acc6f136ed78648b5dda60
mateus.borja <mateus.borja@gmail.com>
2020-10-18 19:35:48 -0400

    constants

---
+++ src/com/mateusborja/javadevanalyst/lesson_5_4/Constantes.java
@@ -0,0 +1,18 @@
+//java constantes
+package com.mateusborja.javadevanalyst.lesson_5_4;
+
+public class Constantes {
+	
+	/*
+	 * public = pode ser acessado fora da clase
+	 * static = pertence a classe e nao as instancias dos objetos
+	 * final = valor final, evita que o valor seja alterado, se fosse alterado seria variavel
+	 * tipo de dado = double, int, float, string, etc..
+	 */
+	public static final double MEDIA_DA_PROVA; // nao inicializado
+	
+	static {
+		MEDIA_DA_PROVA = 7.0;
+	}
+
+}
