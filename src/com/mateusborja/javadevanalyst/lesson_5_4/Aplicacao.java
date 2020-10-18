9f1fab5f5728864725284344cdcdd1bec8050dd0
mateus.borja <mateus.borja@gmail.com>
2020-10-18 19:19:15 -0400

    java static modifier

--- src/com/mateusborja/javadevanalyst/lesson_5_4/Aplicacao.java
+++ src/com/mateusborja/javadevanalyst/lesson_5_4/Aplicacao.java
@@ -6,6 +6,18 @@ public class Aplicacao {
 	
 	public static void main(String[] args) {
 		
+		Matematica m = new Matematica();
+		
+		int soma1 = m.somar(5, 10);
+		int subtracao1 = m.subtratir(50, 10);
+		
+		int soma = Matematica.somar(10, 5);
+		int subtracao = Matematica.subtratir(50, 10);
+		
+		
+		System.out.println(soma);
+		System.out.println(subtracao);
+		
 		
 		
 		
--- src/com/mateusborja/javadevanalyst/lesson_5_4/Matematica.java
+++ src/com/mateusborja/javadevanalyst/lesson_5_4/Matematica.java
@@ -3,12 +3,14 @@ package com.mateusborja.javadevanalyst.lesson_5_4;
 
 public class Matematica {
 	
-	public int somar (int a, int b) {
+	public static int somar (int a, int b) {
 		return a + b;
 	}
 	
-	public int subtratir (int a, int b) {
+	public static int subtratir (int a, int b) {
 		return a- b;
 	}
+	
+	
 
 }
