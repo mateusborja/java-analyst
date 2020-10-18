2347a2876dc0211577d3c0f34ace89d1ae0763fb
mateus.borja <mateus.borja@gmail.com>
2020-10-17 22:08:17 -0400

    java modifiers

---
+++ src/com/mateusborja/javadevanalyst/lesson_5_4/Aplicacao.java
@@ -0,0 +1,21 @@
+//java modifiers static
+package com.mateusborja.javadevanalyst.lesson_5_4;
+
+
+public class Aplicacao {
+	
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ src/com/mateusborja/javadevanalyst/lesson_5_4/Matematica.java
@@ -0,0 +1,14 @@
+//java modifiers static
+package com.mateusborja.javadevanalyst.lesson_5_4;
+
+public class Matematica {
+	
+	public int somar (int a, int b) {
+		return a + b;
+	}
+	
+	public int subtratir (int a, int b) {
+		return a- b;
+	}
+
+}
