8325ca5eaca9bebba6e0c33a5f776413b5b64904
mateus.borja <mateus.borja@gmail.com>
2020-10-21 20:38:03 -0400

    java inrehitance

---
+++ src/com/mateusborja/javadevanalyst/lesson_6_1/Celular.java
@@ -0,0 +1,10 @@
+package com.mateusborja.javadevanalyst.lesson_6_1;
+
+public class Celular extends Telefone{
+	
+	public void adicionarDDD (String ddd) {
+		String n = ddd + this.numero;
+		
+	}
+
+}
