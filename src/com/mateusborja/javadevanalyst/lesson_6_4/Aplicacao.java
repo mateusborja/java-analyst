1fd7aef1e40c1f2788a422045dea41660667c993
mateus.borja <mateus.borja@gmail.com>
2020-10-21 22:14:14 -0400

    polymorphism

--- src/com/mateusborja/javadevanalyst/lesson_6_4/Aplicacao.java
+++ src/com/mateusborja/javadevanalyst/lesson_6_4/Aplicacao.java
@@ -2,22 +2,30 @@
 package com.mateusborja.javadevanalyst.lesson_6_4;
 
 public class Aplicacao {
-	
+
 	public static void main(String[] args) {
-		
+
 		Animal a = new Cachorro(); // subclasse same superclasse // Cachorro c = new Cachorro(); >> polymorphism
 		a.falar();
-		
-		Cachorro c = (Cachorro) a; // casting explicit
-		c.morder();
-		
-		
-		
-		
-		
-		
-		
-		
+
+		// use instanceof
+		if (a instanceof Cachorro) {
+
+			Cachorro c = (Cachorro) a; // casting explicit
+			c.morder();
+		}
+
+		Cachorro c = new Cachorro();
+		Gato g = new Gato();
+
+		falar(c);
+		falar(g);
+
+	}
+
+	private static void falar(Animal a) {
+		a.falar();
+
 	}
 
 }
