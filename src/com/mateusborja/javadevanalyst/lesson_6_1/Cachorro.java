0a73519b4aeda527703b78e630b8794343455c01
mateus.borja <mateus.borja@gmail.com>
2020-10-21 20:59:47 -0400

    java inheritance and polymorphism

---
+++ src/com/mateusborja/javadevanalyst/lesson_6_1/Cachorro.java
@@ -0,0 +1,13 @@
+package com.mateusborja.javadevanalyst.lesson_6_1;
+
+public class Cachorro extends Animal {
+	
+	public void falar() {
+		System.out.println("Au au..");
+	}
+	
+	public void morder() {
+		System.out.println("ai ai ai...");
+	}
+
+}
