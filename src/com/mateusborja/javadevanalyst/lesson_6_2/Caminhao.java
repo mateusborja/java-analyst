87bacffd08c2b074ea2b095b108b63b224a31f2b
mateus.borja <mateus.borja@gmail.com>
2020-10-21 21:36:44 -0400

    inheritance example car

---
+++ src/com/mateusborja/javadevanalyst/lesson_6_2/Caminhao.java
@@ -0,0 +1,11 @@
+package com.mateusborja.javadevanalyst.lesson_6_2;
+
+public class Caminhao extends Veiculo {
+	
+	@Override 
+	public void buzinar() {
+		// TODO Auto-generated method stub
+		System.out.println("caminhao >>> fom fom");
+	}
+
+}
---
+++ src/com/mateusborja/javadevanalyst/lesson_6_2/Motocicleta.java
@@ -0,0 +1,9 @@
+package com.mateusborja.javadevanalyst.lesson_6_2;
+
+public class Motocicleta extends Veiculo {
+	
+	public void empinar() {
+		System.out.println("Uhuu!!!");
+	}
+
+}
