c9db8f8ad233329f0062312b2296b8bc34502ce7
mateus.borja <mateus.borja@gmail.com>
2020-11-03 22:03:48 -0400

    java interfaces example

---
+++ src/com/mateusborja/javadevanalyst/lesson_7_3/Automovel.java
@@ -0,0 +1,16 @@
+//java interface - define apenas a assinatura dos metodos
+/*
+ * nao e necessario utilizar o modificador public
+ */
+package com.mateusborja.javadevanalyst.lesson_7_3;
+
+public interface Automovel {
+
+	void virarEsquerda();
+	void virarDireita();
+	void acelerar();
+	void frear();
+	
+	
+
+}
