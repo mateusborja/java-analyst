9cebdc875b95e4339f1815bf9baf5e3ffad1955c
Mateus Borja <mateus.borja@gmail.com>
2020-11-26 08:44:51 -0400

    classe porsche implementa Automovel

---
+++ src/com/mateusborja/javadevanalyst/lesson_7_4/Porsche.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+
+package com.mateusborja.javadevanalyst.lesson_7_4;
+
+public class Porsche implements Automovel {
+
+	@Override
+	public void acelear() {
+		System.out.println("Porsche Acelerou!");
+		
+	}
+
+	@Override
+	public void frear() {
+		System.out.println("Porsche Freou!");
+		
+	}
+	
+	
+
+}
