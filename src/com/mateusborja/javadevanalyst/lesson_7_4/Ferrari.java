2cd72fdcaa8f00d85a06857c6d1bd4db05f16fe5
Mateus Borja <mateus.borja@gmail.com>
2020-11-26 08:44:36 -0400

    classe ferrari implementa Automovel

---
+++ src/com/mateusborja/javadevanalyst/lesson_7_4/Ferrari.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+
+package com.mateusborja.javadevanalyst.lesson_7_4;
+
+public class Ferrari implements Automovel{
+
+	@Override
+	public void acelear() {
+		System.out.println("Ferrari Acelerou!");
+		
+	}
+
+	@Override
+	public void frear() {
+		System.out.println("Ferrari Freou!");
+		
+	}
+	
+	
+
+}
